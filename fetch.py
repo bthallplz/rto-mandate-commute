cef7572a1143651fc137726a0924c8f2de2dcd80
Brandon Perez-Hall <brandon@tacticaltypos.net>
2025-05-11 08:50:27 -0700

    Move things around as needed and set up TODOs

--- fetch.py
+++ fetch.py
@@ -4,126 +4,47 @@
 
 from datetime import datetime
 
-import feedparser
-import re
 import sqlite3
 import sys
 import time
 
-MAX_ENTRIES_PER_FEED = 8
-
-force = False
-
-if len(sys.argv) > 1 and sys.argv[1] == '--force':
+# TODO: Repurpose this for getting and sticking in the API key for the Google Maps API
+if len(sys.argv) > 1:
     force = True
 
-conn = sqlite3.connect('feeds.db')
-conn.row_factory = sqlite3.Row
+# TODO: Read in the Google Maps API key via an argument, so we don't leak the API key
 
-c = conn.cursor()
+database = sqlite3.connect("commute_data.db")
+database.row_factory = sqlite3.Row
 
-c.execute('SELECT * FROM feeds')
+c = database.cursor()
 
-author_regexp = re.compile(r'noreply@blogger.com \((.*)\)$')
+c.execute("SELECT * FROM commutes")
 
-for feed in c.fetchall():
-    print(feed['name'])
-
-    etag = feed['etag']
-    modified = feed['modified']
-
-    if force:
-        etag = 0
-        modified = 0
-
-    url = feed['url']
+for commute in c.fetchall():
+    print(commute["name"])
 
     try:
-        # etag and modified tell when we last checked so can get only newer
-        # posts
-        data = feedparser.parse(url, etag=etag, modified=modified)
-
-        if data.bozo and 'link' not in data.feed:
-            print(data.bozo)
-            print(data.bozo_exception)
-            print(data.feed)
-            continue
-
-        if 'etag' not in data:
-            data.etag = 0
-        if 'modified' not in data:
-            data.modified = 0
-        if 'status' not in data:
-            data.status = 200
-
-        # if 304 then no data fetched
-        if 'title' in data.feed:
-            title = data.feed.title
-        else:
-            title = feed['title']
-        if 'link' in data.feed:
-            link = data.feed.link
-        else:
-            link = feed['blog_url']
-
-        print(f'''
-ETag:      {data.etag}
-Modified:  {data.modified}
-Status:    {data.status}
-Posts:     {len(data.entries)}
-''')
-        if data.status == 301:  # permanent redirection
-            url = data.href
-
-        c.execute('''UPDATE feeds
-                     SET etag=?, modified=?, url=?, title=?, blog_url=?
-                     WHERE id=?''',
-                  (data.etag, data.modified, url, title, link, feed['id'],))
-
-        # if we got all posts then drop all stored ones
-        if data.status == 200:
-            c.execute('DELETE FROM posts WHERE feed_id=?', (feed['id'],))
-
-        counter = 0
-        for post in data.entries:
-
-            if 'content' in post:
-                content = post.content[0].value
-            elif 'summary' in post:
-                content = post.summary
-
-            if 'author' not in post:
-                author = feed['name']
-            else:
-                author = post.author
-
-            # Blogger
-            author = author_regexp.sub(r'\1', author)
-
-            try:
-                published = datetime.fromtimestamp(
-                    time.mktime(post.published_parsed[:8] + (-1,)))
-            except AttributeError:
-                published = datetime.fromtimestamp(
-                    time.mktime(post.updated_parsed[:8] + (-1,)))
-
-            try:
-                c.execute('''INSERT INTO posts (feed_id, title, post, url,
-                        author, published_date) VALUES (?, ?, ?, ?, ?, ?)''',
-                        (feed['id'], post.title, content, post.link, author,
-                        published))
-            except ValueError:
-                pass
-
-            counter += 1
-            if counter == MAX_ENTRIES_PER_FEED:
-                break
+        # TODO: Insert the data fields from the Google Maps API response into the db
+        """
+        TODO: Use this for current time:
+            `datetime.strftime(datetime.now(),
+                "%d %B %Y %H:%M")`
+        """
+        try:
+            c.execute(
+                # TODO: Add in other data fields we're gathering from API response + time of initiating all commutes (this time around)
+                """INSERT INTO trips (commute_id) VALUES (?)""",
+                (commute["id"],) # Seems to require a tuple, so needs a trailing comma if only one element
+            )
+        except ValueError:
+            pass
 
     except RuntimeError:
-        print('failed')
+        print("failed")
         pass
 
-    print(' ')
+    print(" ")
 
-conn.commit()
-conn.close()
+database.commit()
+database.close()
--- init.py
+++ init.py
@@ -6,16 +6,12 @@ import configparser
 import sqlite3
 import sys
 
-conn = sqlite3.connect('feeds.db')
+database = sqlite3.connect("commute_data.db")
 
-c = conn.cursor()
+c = database.cursor()
 
-c.execute('''CREATE TABLE settings
-             (name text,
-              url text)
-          ''')
-
-c.execute('''CREATE TABLE feeds
+c.execute(
+    """CREATE TABLE commutes
              (id integer primary key autoincrement,
               name text,
               title text,
@@ -23,28 +19,41 @@ c.execute('''CREATE TABLE feeds
               url text,
               etag text,
               modified text)
-          ''')
+          """
+)
 
-c.execute('''CREATE TABLE posts
+c.execute(
+    """CREATE TABLE trips
              (id integer primary key autoincrement,
-             feed_id integer,
+             commute_id integer,
              author text,
              title text,
              post text,
              url text,
              published_date datetime)
-          ''')
+          """
+)
 
+# TODO: Read in the configs for commutes
 if len(sys.argv) > 1:
+    """
+    TODO: See if there's multiple system arguments.
+        Check if the arguments matche some filename patterns,
+            depending on which filename it matches, if any, do a certain thing
+                (like setting some commutes' info to `private` visibility)
+    """
 
     config = configparser.ConfigParser()
     config.read(sys.argv[1])
 
+    # TODO: Plan how the commutes .ini files should work/be structured
     for section in config.sections():
-        if section == 'Planet':
+        if section == "Planet":
             continue
         print(f"{section} {config[section]['name']}")
-        c.execute(f'''INSERT INTO feeds (name, url) VALUES
-                  ("{config[section]['name']}", "{section}")''')
+        c.execute(
+            f"""INSERT INTO commutes (name, url) VALUES
+                  ("{config[section]['name']}", "{section}")"""
+        )
 
-conn.commit()
+database.commit()
