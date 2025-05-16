4c931594fe001e0c7a964e0e52dacd9919596797
Brandon Perez-Hall <brandon@tacticaltypos.net>
2025-05-16 07:13:38 -0700

    Test getting a Google Maps API response and storing the value in db + datetime fix

--- fetch.py
+++ fetch.py
@@ -7,44 +7,87 @@ from datetime import datetime
 import sqlite3
 import sys
 import time
+import googlemaps
 
-# TODO: Repurpose this for getting and sticking in the API key for the Google Maps API
+
 if len(sys.argv) > 1:
-    force = True
+    
+    gmaps = googlemaps.Client(key=sys.argv[1]) # Setting the Google Maps API key via an argument we pass to the script
 
-# TODO: Read in the Google Maps API key via an argument, so we don't leak the API key
+    if sys.argv[2] == "to-work":
+        print('Going to work!\n')
+    
+    elif sys.argv[2] == "to-home":
+        print('Going home! Finally!\n')
+    
+    else:
+        print(
+"""You need to set a second argument for the fetch.py script that specifies what the destination will be.
+Say "to-work" if we're headed to work, or "to-home" if we're headed home."""
+)
 
-database = sqlite3.connect("commute_data.db")
-database.row_factory = sqlite3.Row
+    database = sqlite3.connect("commute_data.db")
+    database.row_factory = sqlite3.Row
 
-c = database.cursor()
+    c = database.cursor()
 
-c.execute("SELECT * FROM commutes")
+    c.execute("SELECT * FROM commutes")
+    
+    now = datetime.now()
 
-for commute in c.fetchall():
-    print(commute["name"])
+    # Adapt the dates so we can continue to read them into SQLite
+    def adapt_datetime_iso(val):
+        """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
+        return val.isoformat()
 
-    try:
-        # TODO: Insert the data fields from the Google Maps API response into the db
-        """
-        TODO: Use this for current time:
-            `datetime.strftime(datetime.now(),
-                "%d %B %Y %H:%M")`
-        """
+    sqlite3.register_adapter(datetime, adapt_datetime_iso)
+
+    # Request the commute time in seconds from the Google Maps API
+    def get_travel_time(from_loc, to_loc):
+            directions_result = gmaps.directions(from_loc,
+                                                to_loc,
+                                                mode="driving",
+                                                departure_time=now)
+
+            return directions_result[0]['legs'][0]['duration_in_traffic']['value'] # In seconds
+    
+    
+    for commute in c.fetchall():
+        print(commute["name"])
+        
+        home_to_work = 61 # Dummy amount of seconds for us to use while testing this, instead of making a request to the API - use get_travel_time() when live
+        
+        work_to_home = 161 # Dummy amount of seconds for us to use while testing this, instead of making a request to the API - use get_travel_time() when live
+
+        print(
+f'''Current time to work from home: {home_to_work / 60} minutes
+
+Current time to home from work: {work_to_home / 60} minutes''')
+
         try:
-            c.execute(
-                # TODO: Add in other data fields we're gathering from API response + time of initiating all commutes (this time around)
-                """INSERT INTO trips (commute_id, duration_in_traffic_seconds) VALUES (?, ?)""",
-                (commute["id"],2) # Seems to require a tuple, so needs a trailing comma if only one element
-            )
-        except ValueError:
+            # TODO: Insert the data fields from the Google Maps API response into the db
+            """
+            TODO: Use this for current time:
+                `datetime.strftime(datetime.now(),
+                    "%d %B %Y %H:%M")`
+            """
+            try:
+                c.execute(
+                    # TODO: Add in other data fields we're gathering from API response + time of initiating all commutes (this time around)
+                    """INSERT INTO trips (commute_id, duration_in_traffic_seconds, published_date) VALUES (?, ?, ?)""",
+                    (commute["id"],home_to_work, now) # Seems to require a tuple, so needs a trailing comma if only one element
+                )
+            except ValueError:
+                pass
+
+        except RuntimeError:
+            print("failed")
             pass
 
-    except RuntimeError:
-        print("failed")
-        pass
+        print(" ")
 
-    print(" ")
+    database.commit()
+    database.close()
 
-database.commit()
-database.close()
+else:
+    print("Please provide as an argument along with this `fetch.py` script the Google Maps API key we should use when gathering commute times.")
